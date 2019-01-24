f96a692c8333aa2dceea2198e529c9d029227ff9
Avaer Kazmer <a@modules.io>
2019-01-24 10:20:23 -0500

    Small Window.js state refactoring

--- src/index.js
+++ src/index.js
@@ -369,26 +369,6 @@ const localQuaternion = new THREE.Quaternion();
 const localMatrix = new THREE.Matrix4();
 const localMatrix2 = new THREE.Matrix4();
 
-const vrPresentState = {
-  vrContext: null,
-  isPresenting: false,
-  system: null,
-  oculusSystem: null,
-  compositor: null,
-  glContext: null,
-  msFbo: null,
-  msTex: null,
-  msDepthTex: null,
-  fbo: null,
-  tex: null,
-  depthTex: null,
-  cleanups: null,
-  hasPose: false,
-  lmContext: null,
-  layers: [],
-};
-GlobalContext.vrPresentState = vrPresentState;
-
 class XRState {
   constructor() {
     const sab = new SharedArrayBuffer(4*1024);
@@ -472,6 +452,49 @@ class XRState {
   }
 }
 const xrState = GlobalContext.xrState = new XRState();
+
+const vrPresentState = {
+  vrContext: null,
+  isPresenting: false,
+  system: null,
+  oculusSystem: null,
+  compositor: null,
+  glContext: null,
+  msFbo: null,
+  msTex: null,
+  msDepthTex: null,
+  fbo: null,
+  tex: null,
+  depthTex: null,
+  cleanups: null,
+  hasPose: false,
+  lmContext: null,
+  layers: [],
+};
+GlobalContext.vrPresentState = vrPresentState;
+
+const mlPresentState = {
+  mlContext: null,
+  mlFbo: null,
+  mlTex: null,
+  mlDepthTex: null,
+  mlMsFbo: null,
+  mlMsTex: null,
+  mlMsDepthTex: null,
+  mlGlContext: null,
+  mlCleanups: null,
+  mlHasPose: false,
+  layers: [],
+};
+GlobalContext.mlPresentState = mlPresentState;
+
+const fakePresentState = {
+  fakeVrDisplay: null,
+  layers: [],
+};
+GlobalContext.fakePresentState = fakePresentState;
+GlobalContext.fakeVrDisplayEnabled = false;
+
 if (nativeBindings.nativeOculusVR) {
   nativeBindings.nativeOculusVR.requestPresent = function (layers) {
     const layer = layers.find(layer => layer && layer.source && layer.source.tagName === 'CANVAS');
@@ -740,20 +763,6 @@ if (nativeBindings.nativeOpenVR) {
     return Promise.resolve();
   };
 }
-const mlPresentState = {
-  mlContext: null,
-  mlFbo: null,
-  mlTex: null,
-  mlDepthTex: null,
-  mlMsFbo: null,
-  mlMsTex: null,
-  mlMsDepthTex: null,
-  mlGlContext: null,
-  mlCleanups: null,
-  mlHasPose: false,
-  layers: [],
-};
-GlobalContext.mlPresentState = mlPresentState;
 
 if (nativeBindings.nativeMl) {
   mlPresentState.mlContext = new nativeBindings.nativeMl();
@@ -968,12 +977,6 @@ if (nativeBindings.nativeMl) {
     s.on('error', () => {});
   }
 }
-
-const fakePresentState = {
-  fakeVrDisplay: null,
-  layers: [],
-};
-GlobalContext.fakePresentState = fakePresentState;
 
 nativeBindings.nativeWindow.setEventHandler((type, data) => {
   const {windowHandle} = data;
