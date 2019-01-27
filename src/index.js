04793179e716e1f674fe1d774cff399fcdf34fb5
Avaer Kazmer <a@modules.io>
2019-01-26 22:10:22 -0500

    Move windows tracking to index.js

--- src/index.js
+++ src/index.js
@@ -1844,6 +1844,8 @@ const _bindWindow = (window, newWindowCb) => {
       process.exit();
     });
   }
+  
+  windows.push(window);
   window.addEventListener('destroy', e => {
     const {window} = e;
     for (let i = 0; i < contexts.length; i++) {
@@ -1852,7 +1854,10 @@ const _bindWindow = (window, newWindowCb) => {
         context.destroy();
       }
     }
+    
+    windows.splice(GlobalContext.windows.indexOf(window), 1);
   });
+  
   window.addEventListener('error', err => {
     console.warn('got error', err);
   });
