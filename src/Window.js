f3ee53a9aa2bd1590d57478a0db02a3030d17b0f
Avaer Kazmer <a@modules.io>
2019-01-28 16:26:55 -0500

    Handle xrState transfer to child windows

--- src/Window.js
+++ src/Window.js
@@ -11,7 +11,14 @@ const util = require('util');
 const {URL} = url;
 const {TextEncoder, TextDecoder} = util;
 const {performance} = require('perf_hooks');
-const {workerData: {args: options}} = require('worker_threads');
+const {
+  workerData: {
+    args: {
+      options,
+      xrState,
+    },
+  },
+} = require('worker_threads');
 
 const {FileReader} = require('./File.js');
 
@@ -96,8 +103,9 @@ const {
   nativeWindow,
 } = bindings;
 
-GlobalContext.args = {};
-GlobalContext.version = '';
+// GlobalContext.args = {};
+// GlobalContext.version = '';
+GlobalContext.xrState = xrState;
 
 // Class imports.
 const {_parseDocument, _parseDocumentAst, Document, DocumentFragment, DocumentType, DOMImplementation, initDocument} = require('./Document');
