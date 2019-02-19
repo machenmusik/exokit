560e98382e059a84de75f0789340d20206c61f33
Avaer Kazmer <a@modules.io>
2019-02-19 09:10:14 -0500

    Bugfix xrOffset SharedArrayBuffer construction across windows

--- src/DOM.js
+++ src/DOM.js
@@ -2016,7 +2016,7 @@ class HTMLIFrameElement extends HTMLSrcableElement {
                     top,
                     htmlString,
                     hidden: this.d === 3,
-                    xrOffset: this.xrOffset,
+                    xrOffsetBuffer: this.xrOffset._buffer,
                   });
 
                   this.contentWindow = contentWindow;
