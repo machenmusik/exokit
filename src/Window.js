b10e191722bd0aa67a121e2e0e5396cb1d4ed467
Avaer Kazmer <a@modules.io>
2019-02-21 16:56:28 -0500

    Bugfix tickAnimationFrame sync consumption from child windows

--- src/Window.js
+++ src/Window.js
@@ -1082,9 +1082,10 @@ const _normalizeUrl = utils._makeNormalizeUrl(options.baseUrl);
   };
   const _tickAnimationFrameRaf = top => async () => {
     const childSyncs = (await Promise.all(windows.map(window => window.tickAnimationFrame(GlobalContext.contexts.length > 0 ? 'child' : 'top')))).flat();
-    for (let i = 0; i < GlobalContext.contexts.length; i++) {
-      const context = GlobalContext.contexts[i];
-      context.setPrereqSyncs && context.setPrereqSyncs(childSyncs);
+    for (let i = 0; i < childSyncs.length; i++) {
+      const childSync = childSyncs[i];
+      nativeWindow.waitSync(childSync);
+      nativeWindow.deleteSync(childSync);
     }
 
     if (rafCbs.length > 0) {
@@ -1125,9 +1126,6 @@ const _normalizeUrl = utils._makeNormalizeUrl(options.baseUrl);
       }
 
       context.clearPrereqSyncs && context.clearPrereqSyncs();
-    }
-    for (let i = 0; i < childSyncs.length; i++) {
-      nativeWindow.deleteSync(childSyncs[i]);
     }
     return syncs;
   };
@@ -1510,7 +1508,7 @@ const _normalizeUrl = utils._makeNormalizeUrl(options.baseUrl);
         });
       };
     };
-    
+
     const fakeVrDisplay = new FakeVRDisplay(window);
     fakeVrDisplay.onrequestpresent = layers => {
       if (!xrState.isPresenting[0]) {
