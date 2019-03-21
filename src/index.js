ed7ec76da9f6f22280303efbb00a29e6059c6652
Diego Marcos <diego.marcos@gmail.com>
2019-03-21 12:29:12 -0700

    Remove console logging

--- src/index.js
+++ src/index.js
@@ -1332,10 +1332,6 @@ const _startRenderLoop = () => {
 
         localVector.toArray(xrState.position);
         localQuaternion.toArray(xrState.orientation);
-        // console.log("CACA " +
-        //   xrState.positon[0] + ' ' +
-        //   xrState.positon[1] + ' ' +
-        //   xrState.positon[2] + ' '  );
       } else {
 
         _normalizeMatrixArray(localFloat32Array);
