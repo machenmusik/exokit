dadce7cda419e91d4a91291b2f1a768a067f03a3
Avaer Kazmer <a@modules.io>
2019-03-26 23:32:20 -0400

    Clean up MacOS event polling

--- src/index.js
+++ src/index.js
@@ -236,7 +236,7 @@ const _startTopRenderLoop = () => {
   };
   const TIMESTAMP_FRAMES = 100;
 
-  nativeBindings.nativeWindow.registerPollEvents && nativeBindings.nativeWindow.registerPollEvents();
+  nativeBindings.nativeWindow.registerEventHandler && nativeBindings.nativeWindow.registerEventHandler();
 
   const _topRenderLoop = async () => {
     if (args.performance) {
@@ -280,7 +280,8 @@ const _startTopRenderLoop = () => {
       timestamps.last = now;
     }
 
-    // update media frames
+    // update events
+    nativeBindings.nativeWindow.pollEvents && nativeBindings.nativeWindow.pollEvents();
     nativeBindings.nativeVideo.Video.updateAll();
     nativeBindings.nativeBrowser.Browser.updateAll();
 
